3f1ff2cd0bfd0eb7ce4e63fa11fbd6ad01952e08
johncarlodeveloepr <2023100305@bulsu.edu.ph>
2025-10-06 19:47:27 +0800

    create filter class for SP and implement logging

--- src/CRM/SmartMoving/Pages/InsightsPage/SalespersonPerformance.py
+++ src/CRM/SmartMoving/Pages/InsightsPage/SalespersonPerformance.py
@@ -1,28 +1,43 @@
 from src.CRM.SmartMoving.Pages.InsightsPage.InsightsPage import InsightsPage
-from src.Chrome.IDriver import IDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from src.CRM.SmartMoving.Filters.SalespersonPerformanceDateTypeFilter import SalespersonPerformanceDateTypeFilter
+from src.CRM.SmartMoving.Filters.CalendarFilter import CalendarFilter
+from selenium.common.exceptions import TimeoutException
 
 class SalespersonPerformance(InsightsPage):
-    def __init__(self):
-        pass
+    def __init__(self, calendar_filter: CalendarFilter, date_type_filter: SalespersonPerformanceDateTypeFilter):
+        self.calendar_filter = calendar_filter
+        self.date_type_filter = date_type_filter
 
-    def get_bad_leads(self) -> int:
+    def get_bad_leads(self) -> int | None:
         xpath = "//span[normalize-space(text())='Bad']/following-sibling::h2/span[1]"
-        bad_leads_el = WebDriverWait(self._driver, 60).until(
-            EC.visibility_of_element_located((By.XPATH, xpath))
-        )
-        return int(bad_leads_el.text.strip())
+        self._logger.info("Attempting to get bad leads count...")
+        try:
+            bad_leads_el = WebDriverWait(self._driver, self.DEFAULT_TIMEOUT).until(
+                EC.visibility_of_element_located((By.XPATH, xpath))
+            )
+            return int(bad_leads_el.text.strip())
+        except TimeoutException:
+            self._logger.warning("Failed to get bad leads count under 60 seconds.")
+        except ValueError:
+            self._logger.warning(f"Unable to convert {bad_leads_el.text} to int.")
 
 
-    def get_bad_leads_percentage(self) -> str:
+    def get_bad_leads_percentage(self) -> str | None:
         xpath = "//span[normalize-space(text())='Bad']/following-sibling::h2/span[2]"
-        bad_leads_percentage_el = WebDriverWait(self._driver, 60).until(
-            EC.visibility_of_element_located((By.XPATH, xpath))
-        )
-        return bad_leads_percentage_el.text.strip()
+
+        self._logger.info("Attempting to get bad leads percentage...")
+        try:
+            bad_leads_percentage_el = WebDriverWait(self._driver, self.DEFAULT_TIMEOUT).until(
+                EC.visibility_of_element_located((By.XPATH, xpath))
+            )
+            return bad_leads_percentage_el.text.strip()
+        except TimeoutException:
+            self._logger.warning("Failed to get bad leads percentage under 60 seconds.")
+        except ValueError:
+            self._logger.warning(f"Unable to convert {bad_leads_percentage_el.text} to int.")
 
 
         
